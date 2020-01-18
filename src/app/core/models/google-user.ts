446f754d2fb8ec0460248c495915b4d44e880eea
Patrick Murphy <pmurphy1413@gmail.com>
2020-01-18 13:33:24 -0600

    Added auth

---
+++ src/app/core/models/google-user.ts
@@ -0,0 +1,6 @@
+export interface GoogleUser {
+    uid: string;
+    email: string;
+    photoURL?: string;
+    displayName?: string;
+}
--- src/app/modules/landing/landing.component.ts
+++ src/app/modules/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from 'src/app/core/firebase/firebase.service';
 
 @Component({
   selector: 'app-landing',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(public fb: FirebaseService) { }
 
   ngOnInit() {
   }
