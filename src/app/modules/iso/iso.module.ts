b99053393032f7eeb54d1698685cf47fca4a853e
LukeAVanDrie <42616066+LukeAVanDrie@users.noreply.github.com>
2020-01-18 12:58:14 -0600

    started iso card

--- src/app/modules/iso/iso.module.ts
+++ src/app/modules/iso/iso.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { IsoComponent } from './iso.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { IsoCardComponent } from './components/iso-card/iso-card.component';
 
 @NgModule({
-  declarations: [IsoComponent],
+  declarations: [IsoComponent, IsoCardComponent],
   imports: [
     CommonModule,
     SharedModule
