2a7263bcd722dcd4aeec0bc38f0de32a1e3c67ad
Jacob Sampson <jacob.sampson@huskers.unl.edu>
2020-01-18 20:12:23 -0600

    Updated styling on Google Maps"

--- src/app/core/google-maps/google-maps.service.ts
+++ src/app/core/google-maps/google-maps.service.ts
@@ -6,28 +6,26 @@ import { Injectable } from '@angular/core';
 export class GoogleMapsService {
   private DIR_BASE = 'https://www.google.com/maps/dir/?api=1';
 
-
   constructor() { }
 
   /**
    * Gets google maps url that will display directions from specificied from location to specified to location
-   * 
-   * @param from 
-   * @param to 
+   *
+   * @param from
+   * @param to
    */
-  public getDirectionsFromToUrl(from: string, to: string) : string {
-    let string = this.DIR_BASE + '&origin=' + encodeURIComponent(from);
-    string += '&destination=' + encodeURIComponent(to);
-    return string;
+  public getDirectionsFromToUrl(from: string, to: string): string {
+    const str = this.DIR_BASE + '&origin=' + encodeURIComponent(from);
+    str.concat('&destination=' + encodeURIComponent(to));
+    return str;
   }
 
   /**
    * Get google maps url that will display directions from user's current location to specified to location
-   * 
-   * @param to 
+   *
+   * @param to
    */
-  public getDirectionsToUrl(to: string) : string {
-    let string = this.DIR_BASE + '&destination=' + encodeURIComponent(to);
-    return string;
+  public getDirectionsToUrl(to: string): string {
+    return this.DIR_BASE + '&destination=' + encodeURIComponent(to);
   }
 }
